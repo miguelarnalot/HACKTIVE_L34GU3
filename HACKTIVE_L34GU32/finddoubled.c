6d637e5b9779f116ad898cf0b1a4119d7fac45c2
miguel <miguel.arnalot-borao@epitech.eu>
2021-05-29 19:10:45 +0200

    finddoubled

---
+++ HACKTIVE_L34GU32/finddoubled.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int singleNumber(int* nums, int numsSize)
+{
+   int i = 0;
+   int counter = 0;
+   int j = 0;
+   int num = 0;
+
+   while (i < numsSize) {
+      num = nums[i];
+      while (j < numsSize) {
+         if (num == nums[j])
+            counter++;
+      j++;
+      }
+      j = 0;
+      if (counter == 1)
+         return (num);
+      i++;
+      counter = 0;
+   }
+   // printf("skip, nums[i] = %d %d\n", nums[i], i);
+   // printf("skip, nums[counter] = %d %d\n\n", nums[counter], counter);
+
+}
+
+int main (void)
+{
+   int numsSize = 5;
+   int *nums =  malloc(sizeof(int) * numsSize);
+
+   nums[0] = 6;
+   nums[1] = 3;
+   nums[2] = 1;
+   nums[3] = 3;
+   nums[4] = 6;
+
+   
+
+   return singleNumber(nums, numsSize);
+}
